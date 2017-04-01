73b9504e8c2645fe6ef20f304286b285ab52f42c
Natalka <nazarjazz@wp.pl>
2017-04-01 19:44:13 +0200

    onClick methods added, buttons styled

--- app/src/main/java/pl/nataliana/apkaoli/MainActivity.java
+++ app/src/main/java/pl/nataliana/apkaoli/MainActivity.java
@@ -1,7 +1,11 @@
 package pl.nataliana.apkaoli;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openGit(View view) {
+        Intent intentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.github.com/natansalda"));
+        try {
+            startActivity(intentImplicit);
+        } catch (ActivityNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void openLinkedIn(View view) {
+        Intent intentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/natalia-nazaruk-1b9a229a/"));
+        try {
+            startActivity(intentImplicit);
+        } catch (ActivityNotFoundException ex) {
+            ex.printStackTrace();
+        }
     }
 }
 
